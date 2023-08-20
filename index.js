fd4b4e5e59ee2c26a5cd2a7bc2cc8a4235118071
Soliu Adeosun <sawlew@gmail.com>
2023-08-21 00:06:08 +0100

    done

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ function notification(){
     let notice = document.getElementById("notify");
     let tabs = document.querySelectorAll("#line");
     let dot = document.querySelectorAll("#dot1");
+    let textcolor = document.querySelectorAll("#textColor");
+
     tabs.forEach(element => {
         element.style.background = "white";
     })
     dot.forEach(element => {
         element.style.display = "none";
+    })
+
+    textcolor.forEach(element => {
+        element.style.color = "gray";
     })
 
     notice.innerHTML = "0";
